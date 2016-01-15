1ae74758bd96c3c55f650cb6c6296fc398f224b9
Alexander Nickel <alexander.nickel@flagbit.de>
2016-01-15 14:10:24 +0100

    corrected path's

--- assets/app.js
+++ assets/app.js
@@ -239,13 +239,13 @@
   app.config(function($routeProvider) {
     return $routeProvider.when("/finder", {
       controller: "FinderController",
-      templateUrl: "/assets/app/views/finder/index.html"
+      templateUrl: "assets/app/views/finder/index.html"
     }).when("/filter", {
       controller: "FilterController",
-      templateUrl: "/assets/app/views/filter/index.html"
+      templateUrl: "assets/app/views/filter/index.html"
     }).when("/register", {
       controller: "RegisterController",
-      templateUrl: "/assets/app/views/register/index.html"
+      templateUrl: "assets/app/views/register/index.html"
     }).otherwise({
       redirectTo: "/finder"
     });
